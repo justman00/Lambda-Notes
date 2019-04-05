a2a788bd1347b1cb8ef9afc23269588a286b75c7
Vlad <vovaturcan4@gmail.com>
2019-04-05 22:58:11 +0300

    confugured webpack and babe

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,52 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const port = process.env.PORT || 3000;
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.[hash].js",
     path: path.resolve(__dirname, "dist")
   },
-  mode: "development"
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              camelCase: true,
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      favicon: "public/favicon.ico"
+    })
+  ],
+  devServer: {
+    host: "localhost",
+    port,
+    historyApiFallback: true,
+    open: true
+  }
 };
